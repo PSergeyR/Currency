a50529d9c21176e00c13aae2888c0f8ba907523f
PSergeyR <94289247+PSergeyR@users.noreply.github.com>
2022-03-07 12:40:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+from datetime import datetime, date
+
+
+date = str(datetime.now()).split(' ')
+date_now = date[0].replace('-', '/').split('/')
+
+
+cbr_adress = f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={date_now[2]+ "/" + date_now[1] + "/" + date_now[0]}'
+xml_page = requests.get(cbr_adress)
+soup = BeautifulSoup(xml_page.content, 'lxml')
+#print(soup)
+
+result = str(soup)
+lst = result.split('</valute>')
+split_lst = str(lst[10])
+#print(split_lst)
+res2 = split_lst.replace('>', ' ').split('<')
+print((res2[8] + ','+ res2[10]).replace('name', '').replace('value', ':'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
