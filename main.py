c733b3f2156b972388c2bf8f9077e85cb8594802
PSergeyR <mapf44@mail.ru>
2022-03-08 15:34:26 +0500

    Добавил чтение из файла и бота телеграмм

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import telebot
 
 
 def date_today():
@@ -24,19 +25,47 @@ def course(valute):
     res2 = split_lst.replace('>', ' ').split('<')  # Отрезаем все лишнее
     return (res2[8] + ',' + res2[10]).replace('name', '').replace('value', ':')  # Делаем ноормальную выдачу
 
+open_token = open('token.txt', 'r')
+str_token = open_token.read()
+bot = telebot.TeleBot(str_token)
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, course(10))
+
+
+def token():
+    open_token = open('token.txt', 'r')  # Читаем токен из файла
+    str_token = open_token.read()
+    open_token.close()  # Закрываем файл
+    return str_token
+
+# БОТ
+
+
+bot = telebot.TeleBot(token())
+
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, course(10))
+
+# Получение сообщений от юзера
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+        bot.send_message(message.chat.id, 'Сегодня : ' + date_today())
+
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
+
+
+
 
 if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
     print(course(10))  # 10- USA
     print(f'Текущая дата: ' + date_today())
-
-
-
-
-
-
-
-
-
-
-
 
